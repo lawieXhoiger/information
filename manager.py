84cdac75143de71e3f71ec9c7064bd42d3d818f6
lawieXhoiger <zengfangdelan13@163.com>
2021-04-13 19:21:09 +0800

    项目的配置&数据库的配置

--- manager.py
+++ manager.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+class Config(object):
+    DEBUG=True
+    SQLALCHEMY_DATABASE_URI="mysql://root:mysql@127.0.0.1:3306/information27"
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
 
+
+db=SQLAlchemy(app)
 
 @app.route("/")
 def index():
@@ -9,4 +20,4 @@ def index():
 
 
 if __name__ == "__main__":
-     app.run(debug=True)
+     app.run()
