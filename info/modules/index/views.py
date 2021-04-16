2b8e78461cb6852da7c52a35e58e0d006bceaab2
lawieXhoiger <zengfangdelan13@163.com>
2021-04-16 08:37:39 +0800

    运通信后台返回验证码

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,10 +1,20 @@
+from flask import current_app
+from flask import render_template
+
 from info import redis_store
 from . import index_blu
 
 
 @index_blu.route("/")
 def index():
-    # redis_store.set('name','itcast')
-    redis_store.set('name','itheima')
-    # 向redis中取一个值
-    return 'index'
+    # # redis_store.set('name','itcast')
+    # redis_store.set('name','itheima')
+    # # 向redis中取一个值
+    # return 'index'
+    return render_template('news/index.html')
+
+# 打开网页时候，浏览器会默认取请求根路径+/favicon.ico做网站标签的小图标
+# send_static_file是flask去查指定的文件调用的方法
+@index_blu.route('/favicon.ico')
+def favicon():
+    return current_app.send_static_file('news/favicon.ico')
