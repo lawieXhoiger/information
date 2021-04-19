03ee6532e943837f873223eb854b2f467f1f2cfd
lawieXhoiger <zengfangdelan13@163.com>
2021-04-19 22:21:03 +0800

    是否收藏的前端逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,8 +1,10 @@
+from flask import abort
 from flask import current_app
 from flask import g
 from flask import render_template
 from flask import session
 
+from info import constants
 from info.models import User, News
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
@@ -16,34 +18,39 @@ def news_detail(news_id):
     :return:
     """
 
-
-    # user_id = session.get('user_id', None)
-    # user = None
-    # if user_id:
-    #     try:
-    #         user = User.query.get(user_id)
-    #     except Exception as e:
-    #         current_app.logger.error(e)
-    #
-    # news_list = []
-    # # 右侧的新闻排行的逻辑
-    # try:
-    #     news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
-    # except Exception as e:
-    #     current_app.logger.error(e)
+    user = g.user
+    news_list = []
+    # 右侧的新闻排行的逻辑
+    try:
+        news_list = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
     # 定义一个空字典
-    user=g.user
-    news_list=[]
+
     news_dict_li = []
     for news in news_list:
         news_dict_li.append(news.to_basic_dict())
 
+    # 查询新闻数据
+    news=None
+    try:
+        news=News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
 
+    if not news:
+        # 404错误,后续统一处理
+        abort(404)
+
+    # TODO收藏
+    is_collected=False
     data = {
         # 如果user是真的话就去取值前面的,如果是none就取值None
         "user": user.to_dict() if user else None,
         'news_dict_li': news_dict_li,
-
+        'news':news.to_dict(),
+        'is_collected':is_collected
     }
-
+    # 更新新闻点击次数
+    news.clicks+=1
     return render_template("news/detail.html",data=data)
