ceadd0c5545e36780863a90406c8b4b4f34aaf26
lawieXhoiger <zengfangdelan13@163.com>
2021-04-20 14:04:31 +0800

    收藏和取消收藏代码的后台逻辑实现

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,13 +1,63 @@
-from flask import abort
+from flask import abort, jsonify
 from flask import current_app
 from flask import g
 from flask import render_template
+from flask import request
 from flask import session
 
 from info import constants
 from info.models import User, News
 from info.modules.news import news_blu
 from info.utils.common import user_login_data
+from info.utils.response_code import RET
+
+
+@news_blu.route("/news_collect",methods=['POST'])
+@user_login_data
+def collect_news():
+    """
+    收藏新闻
+    1.接收参数
+    2.判断参数
+    3.查询新闻
+    :return:
+    """
+    user = g.user
+    news_id=request.json.get("news_id")
+    action =request.json.get("action")
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+    if not news_id:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    if action not in ("collect", "cancel_collect"):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id=int(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据失败")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="新闻数据不存在")
+
+    if action=='cancel_collect':
+        if news in user.collection_news:
+            user.collect_news.remove(news)
+    else:
+        # 收藏
+        if news not in user.collection_news:
+            user.collect_news.append(news)
+
+
+    return jsonify(errno=RET.OK, errmsg="收藏成功")
+
+
 
 
 @news_blu.route('/<int:news_id>')
@@ -44,6 +94,13 @@ def news_detail(news_id):
 
     # TODO收藏
     is_collected=False
+
+    news.clicks += 1
+    # 如果用户已登陆,就判断新闻是否在被用户收藏的列表中
+    if user:
+        if news in user.collection_news:
+            is_collected=True
+
     data = {
         # 如果user是真的话就去取值前面的,如果是none就取值None
         "user": user.to_dict() if user else None,
@@ -52,5 +109,5 @@ def news_detail(news_id):
         'is_collected':is_collected
     }
     # 更新新闻点击次数
-    news.clicks+=1
+
     return render_template("news/detail.html",data=data)
